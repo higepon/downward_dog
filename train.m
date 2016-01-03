17f0e6f78e3f96867958a086981dd821e519e15d
Taro Minowa Higepon <higepon@twitter.com>
2016-01-03 07:31:41 -0800

    Format noise data.

--- train.m
+++ train.m
@@ -1,11 +1,28 @@
-function formatted = formatInput(x, maxNumInput)
-  formatted = (vec(x))(1:maxNumInput);
+function ret = maxInput()
+  ret = 90;
+end
+
+function formatted = formatInput(x)
+  formatted = (vec(x))(1:maxInput());
 end
 
 function input = loadInput(file)
-  input = formatInput(load(file), 80);         
+  input = formatInput(load(file), maxInput());         
 end
 
+# format noise as (maxInput x n) matrix
+function formatted = formatNoiseInnput()
+  x = load("noise.dat");
+  x = vec(x);
+  length(x) / maxInput();
+  num_col = length(x) / maxInput();
+  num_col = floor(num_col);
+  x = x(1:num_col * maxInput);
+  formatted = reshape(x, maxInput, num_col);
+end
+
+noise = formatNoiseInnput();
+size(noise)
 downdog1 = loadInput("downdog1.dat");
 size(downdog1)
 

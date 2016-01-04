e4853ffbeaec284cf2acda8f1be640466b059ec6
Taro Minowa Higepon <higepon@twitter.com>
2016-01-04 05:35:47 -0800

    First iteration worked.

--- train.m
+++ train.m
@@ -61,4 +61,50 @@ function [X_train, y_train, X_cv, y_cv, X_test, y_test] = loadAllData()
   y_test = vertcat(y1_test, y2_test);
 end
 
+input_layer_size  = maxInput();
+hidden_layer_size = 200;
+num_labels = 1;
+
+fprintf("==== Start ====\n");
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+%  After you have completed the assignment, change the MaxIter to a larger
+%  value to see how more training helps.
+options = optimset('MaxIter', 50);
+
+%  You should also try different values of lambda
+lambda = 1;
+
+
 [X_train, y_train, X_cv, y_cv, X_test, y_test] = loadAllData();
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X_train, y_train, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+Theta1
+Theta2
+
+y_cv
+pred = predict(Theta1, Theta2, X_cv)
+
+y_test
+pred = predict(Theta1, Theta2, X_test)
